38f2f27e0df377b131d32d22fb579521eecf3bd9
Pimping <auagfuiawg@mail.ru>
2024-03-09 17:42:51 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bubble Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(637, 9);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(35, 13);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Selection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Insertion";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 493);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Random rand = new Random();
+        public int ArraySize = 500;
+        private int[] array;
+        private PictureBox[] arrayBars;
+        private Timer timer;
+        private int currentIndex;
+        private Label[] arrayLabels;
+        private Action sortingAlgorithm;
+        private Stopwatch stopwatch;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeArray();
+            InitializeArrayBars();
+            InitializeTimer();
+        }
+
+        private int GetArrayRandomSize()
+        {
+            return rand.Next(1, 100);
+        }
+
+        //CREATE ARRAY ↓↓↓↓
+        private void InitializeArray()
+        {
+            array = new int[ArraySize];
+            Random random = new Random();
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                array[i] = random.Next(1, 150);
+            }
+        }
+        //CREATE BARS OF ARRAY ↓↓↓
+        private void InitializeArrayBars()
+        {
+            arrayBars = new PictureBox[ArraySize];
+            arrayLabels = new Label[ArraySize];
+
+            FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                AutoSize = true,
+                Location = new Point(10, 150),
+            };
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                arrayBars[i] = new PictureBox
+                {
+                    Height = array[i],
+                    Width = 30,
+                    BackColor = Color.Black,
+                };
+
+                arrayLabels[i] = new Label
+                {
+                    Text = array[i].ToString(),
+                    AutoSize = true,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                };
+
+                flowLayoutPanel.Controls.Add(arrayBars[i]);
+                flowLayoutPanel.Controls.Add(arrayLabels[i]);
+            }
+
+            Controls.Add(flowLayoutPanel);
+        }
+
+        private void BubbleSort()
+        {
+            for (int j = 0; j < ArraySize - currentIndex - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    Swap(j, j + 1);
+                }
+            }
+        }
+        //CREATE TIMER ↓↓↓
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            //timer.Interval = 200;
+            timer.Tick += Timer_Tick;
+            stopwatch = new Stopwatch();
+        }
+        //SWAP METHOD
+        private void Swap(int index1, int index2)
+        {
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex < ArraySize)
+            {
+                PerformSortingStep();
+                UpdateArrayBars();
+            }
+            else
+            {
+                timer.Stop();
+                stopwatch.Stop();
+                currentIndex = 0;
+                lblTimer.Text = $"Время: {stopwatch.Elapsed.TotalSeconds:F5} сек";
+            }
+        }
+        //FOR SWAPING STEP FOR ALGORITHMS↓↓↓
+        private void PerformSortingStep()
+        {
+            sortingAlgorithm.Invoke();
+            currentIndex++;
+        }
+        //FOR ARRAYS MEASURE ↓↓↓
+        private void UpdateArrayBars()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                arrayBars[i].Height = array[i];
+                arrayLabels[i].Text = array[i].ToString();
+            }
+        }
+
+        private void SelectionSort()
+        {
+            int minIndex = currentIndex;
+            for (int i = currentIndex + 1; i < ArraySize; i++)
+            {
+                if (array[i] < array[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+
+            Swap(currentIndex, minIndex);
+        }
+
+        //SELECTION SORT BUTTON ↓↓↓
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                sortingAlgorithm = SelectionSort;
+                StartSorting();
+            }
+        }
+
+        private void InsertionSort()
+        {
+            int key = array[currentIndex];
+            int j = currentIndex - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+        //DELEGATE FOR SWAPING ALGRORITHMS
+        private void StartSorting()
+        {
+            InitializeArray();
+            currentIndex = 0;
+            timer.Start();
+            stopwatch.Restart();
+        }
+        //INSERTION SORT BUTTON ALGORITHM↓↓↓
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                sortingAlgorithm = InsertionSort;
+                StartSorting();
+            }
+        }
+        //BUBBLE SORT BUTTON ALGORITHM ↓↓↓
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                sortingAlgorithm = BubbleSort;
+                StartSorting();
+            }
+        }
+    }
+}
